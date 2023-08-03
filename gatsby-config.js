40dfe525fa30e086ae0a36ee265941a01616de96
jia-wei-00 <wwwbingo6@gmail.com>
2023-08-04 00:37:25 +0800

    add vercel analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -90,5 +90,12 @@ module.exports = {
         extensions: ["js"],
       },
     },
+    {
+      resolve: "@vercel/gatsby-plugin-vercel-analytics",
+      options: {
+        // (optional) Prints metrics in the console when true
+        debug: false,
+      },
+    },
   ],
 };
